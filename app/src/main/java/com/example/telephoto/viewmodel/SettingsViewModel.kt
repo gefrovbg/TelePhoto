8899d27b8962a45421d406cbf4829a564c5c4086
gefro <gefro47@gmail.com>
2022-05-25 12:21:18 +0200

    master-0004: added animation to icons of bottom navigation menu

--- app/src/main/java/com/example/telephoto/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/example/telephoto/viewmodel/SettingsViewModel.kt
@@ -2,6 +2,7 @@ package com.example.telephoto.viewmodel
 
 import TelegramBotMessageRepositoryAppImpl.Companion.tukTukClient
 import android.content.SharedPreferences
+//import android.util.Log
 import androidx.lifecycle.*
 import com.example.telephoto.data.repository.DescriptionSharedPreferencesRepositoryImpl.Companion.DESC_PREF_NAME
 import com.example.telephoto.data.storage.sharedprefs.SharedPreferencesRepository
@@ -9,6 +10,7 @@ import com.example.telephoto.domain.models.Client
 import com.example.telephoto.domain.models.Token
 import com.example.telephoto.domain.usecase.*
 import com.example.telephoto.presentation.adapters.ClientAdapter
+//import kotlinx.coroutines.*
 
 class SettingsViewModel(
     private val saveTokenToSharedPreferencesUseCase: SaveTokenToSharedPreferencesUseCase,
@@ -48,6 +50,30 @@ class SettingsViewModel(
                 )
             )
         }
+//        Log.d("viewModel", " \nCreated")
+//        viewModelScope.launch {
+//            delay(1000)
+//            val result = withContext(Dispatchers.Default){
+//                val part1 = async {
+//                    delay(1000)
+//                    return@async "Part 1 done!"
+//                }
+//                val part2 = async {
+//                    delay(2000)
+//                    return@async "Part 2 done!"
+//                }
+//                val part3 = async {
+//                    delay(3000)
+//                    return@async "Part 3 done!"
+//                }
+//                val result1 = part1.await()
+//                val result2 = part2.await()
+//                val result3 = part3.await()
+//
+//                return@withContext " \n$result1\n$result2\n$result3"
+//            }
+//            Log.d("viewModel", result)
+//        }
     }
 
     fun saveToken(token: String){
